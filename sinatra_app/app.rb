ec6c11f7d9d3c9aa361aebbf455f93022bbc87c4
cristian-castrillon <cristian.castrillon@gmail.com>
2017-05-23 20:47:28 -0500

    [ADD] Contenido de la segunda clase de sinatra con formas y manejos de errores

--- sinatra_app/app.rb
+++ sinatra_app/app.rb
@@ -1,9 +1,9 @@
 require "sinatra"
 
-# get '/' do
-# 	@producto = "Hola variable de instancia"
-# 	erb :index
-# end
+get '/' do
+	# @producto = "Hola variable de instancia"
+	erb :index
+end
 
 # get '/holamundo' do
 # 	puts params
@@ -15,14 +15,20 @@ require "sinatra"
 # post '/orders' do
 # end
 
-# get '/orders' do
-# 	@orders = [
-# 		{order_id: "o_aerf25",price: 5000 }
-# 		{order_id: "a_aerf25",price: 5000 }
-# 	]
+get '/orders' do
+	@order = {}
 
-# 	erb :orders
-# end
+	@orders = [
+		{order_id: "o_aerf25",price: 5000 },
+		{order_id: "a_aerf25",price: 5000 }
+	]
+
+	@orders.each do |order|
+		@order = order if order[:order_id] == params[:search]
+	end
+
+	erb :orders
+end
 
 # delete '/orders/:order_id' do
 # end
@@ -42,3 +48,46 @@ get '/customers' do
 	erb :customers
 end
 
+get "/foo" do
+	# request.user_agent # Obtiene el browser o cliente usado
+	puts "AGENT = #{request.user_agent}"
+	puts "URL = #{request.url}"
+	puts "PORT = #{request.port}"
+	puts "METHOD = #{request.request_method}"
+	puts "IP = #{request.ip}"
+	puts "HTTP_ACCEPT = #{request.env['HTTP_ACCEPT']}"
+	puts "SERVER_PORT = #{request.env['SERVER_PORT']}"
+	puts "HTTP_USER_AGENT = #{request.env['HTTP_USER_AGENT']}"
+	puts "REQUEST_METHOD = #{request.env['REQUEST_METHOD']}"
+	puts "==" * 10
+	puts "#{request.env}"
+end
+
+get "/login" do
+	erb :login
+end
+
+post "/login" do
+	# puts "Email = #{params[:email]}"
+	# puts "Password = #{params[:password]}"
+	# "Success"
+	# if params[:password] == "abc123"
+	# 	redirect "/user_account/#{params[:email]}"
+	# else
+	# 	erb :login
+	# end
+	if params[:email] == "" || params[:password] ==""
+		@errors = [message: "Email or Password can't be blank"]
+		return error(400, erb(:login))
+	end
+	unless params[:email] == "aaa@aaaa.com" && params[:password] =="abc123"
+		@errors = [message: "Email or password does not match"]
+		return error(400, erb(:login))
+	end
+	redirect "/user_account/#{params[:email]}"
+end
+
+get "/user_account/:email" do
+	@email = params[:email]
+	erb :account
+end
