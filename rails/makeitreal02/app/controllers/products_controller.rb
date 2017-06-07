0cad308e91551b61e5d0233331fbe4bb173a5432
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-06 20:45:22 -0500

    [MOD] Modelos y migraciones

---
+++ rails/makeitreal02/app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to(products_path)
+    else
+      @errors = @product.errors.full_messages
+      render "products/new"
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:sku, :quantity, :name)
+  end
+end
