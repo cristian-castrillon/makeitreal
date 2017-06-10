e63f8cc66fdc740ffbfc7d41db677ee444e5fbf4
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-09 20:45:13 -0500

    [MOD] Pequeños cambios

--- rails/makeitreal02/app/models/user.rb
+++ rails/makeitreal02/app/models/user.rb
@@ -6,10 +6,15 @@ class User < ApplicationRecord
   validates :password, length: { in: 8..16 }
 
   before_validation :sanitize
+  before_create :creating
 
   private
     def sanitize
       username.downcase!
       email.downcase!
+    end
+
+    def creating
+      puts "Creando usuario"
     end
 end
