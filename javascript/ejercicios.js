41131f1fabc440f507b080c4c8058f4fb2350386
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-20 20:49:37 -0500

    [ADD] Tercera clase de Javascript

--- javascript/ejercicios.js
+++ javascript/ejercicios.js
@@ -49,3 +49,190 @@ var student = {
   rollno: 12,
   birthdate: new Date()
 }
+
+function User(name, email) {
+  this.name = name
+  this.email = email
+
+  this.getName = function() {
+    return this.name
+  }
+}
+
+User.prototype.getEmail = function() {
+  return this.email
+};
+
+Array.prototype.average = function() {
+  var avg = 0
+  this.forEach(function(num) {
+    avg += num
+  })
+  return avg / this.length
+};
+
+var comment = {
+  title: '',
+  content: '',
+  titleCapitalized: function(){
+    return this.title.toUpperCase()
+  }
+}
+
+var fullname = 'John Doe';
+var obj = {
+  fullname: 'Colin Ihrig',
+  prop: {
+    fullname : 'Aurelio De Rosa',
+    getFullname: function() {
+      return this.fullname;
+    }
+  }
+};
+
+console.log(obj.prop.getFullname()); // 'Aurelio De Rosa'
+////////////////////////////////////////////////////////////////////////////////////
+
+// function Comment(comment) {
+//   this._comment = comment
+//   this.getComment = function() {
+//     return this._comment
+//   }
+// }
+
+// function Post(title, content) {
+//   this._title = title
+//   this._content = content
+//   this._comments = []
+//   this.getTitle = function() {
+//     return this._title
+//   }
+//   this.getContent = function(){
+//     return this._content
+//   }
+//   this.comment = function(comment) {
+//     this._comments.push(new Comment(comment))
+//   }
+//   this.getComments = function() {
+//     return this._comments
+//   }
+// }
+
+// function Author(name) {
+//   this._name = name
+//   this._posts = []
+//   this.getName = function() {
+//     return this._name
+//   }
+//   this.post = function(title, content) {
+//     this._posts.push(new Post(title, content))
+//   }
+//   this.getPosts = function() {
+//     return this._posts
+//   }
+// }
+
+function PostNotFoundError(message) {
+  this.message = message
+  Error.captureStackTrace(this, this.constructor)
+
+}
+PostNotFoundError.prototype = Object.create(Error.prototype)
+PostNotFoundError.prototype.constructor = PostNotFoundError
+
+function Comment(content) {
+  this.content = content
+}
+
+function Author(email) {
+  this.email = email
+  this.posts = []
+}
+
+Author.prototype.createPost = function(post) {
+  post.setAuthor(this)
+  this.posts.push(post)
+}
+
+Author.prototype.getPosts = function() {
+  return this.posts
+}
+
+Author.prototype.findPostByTitle = function(title) {
+  var found = this.posts.find(function(post) {
+    return post.title === title
+  })
+
+  if (!found) {
+    throw new PostNotFoundError('Post Not Found')
+  }
+
+  return found
+}
+
+function Post(attrs) {
+  this.title = attrs.title
+  this.content = attrs.content
+  this.author = attrs.author
+  this.comments = []
+  this.maxLog = 10
+}
+
+Post.prototype.setAuthor = function(author) {
+  this.author = author
+}
+
+Post.prototype.addComment = function(comment) {
+  if (this.comments.length === this.maxLog) {
+    throw new PostLimitError('Post Limit Reached')
+    return
+  }
+  this.comments.push(comment)
+}
+
+Post.prototype.getComments = function() {
+  return this.comments
+}
+
+var p1 = new Post({
+  title: '',
+  content: ''
+})
+var p2 = new Post({
+  title: '',
+  content: ''
+})
+
+var author = new Author('cc@gm.com')
+author.createPost(p1)
+author.createPost(p2)
+
+var com1 = new com('')
+var com2 = new com('')
+
+p1.addComment(com1)
+p1.addComment(com2)
+
+var com3 = new com('')
+var com4 = new com('')
+
+p2.addComment(com3)
+p2.addComment(com4)
+
+try{
+  author.findPostByTitle('The Lord of')
+} carch (error) {
+  if (error instanceof PostNotFoundError) {
+    console.log('MESSAGE: ', error.message)
+    console.log('STACK: ', error.stack)
+  } else {
+    throw error
+  }
+}
+
+function PostLimitError(message) {
+  this.message = message
+  Error.captureStackTrace(this, this.constructor)
+}
+PostLimitError.prototype = Object.create(Error.prototype)
+PostLimitError.prototype.constructor = PostLimitError
