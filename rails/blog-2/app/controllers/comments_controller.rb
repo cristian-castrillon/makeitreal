5870c6e7b6bbed504d6e60eed45f73e823aa5046
cristian-castrillon <cristian.castrillon@gmail.com>
2017-07-07 20:48:56 -0500

    [MOD] Mostrando comentarios con ajax y javascript

--- rails/blog-2/app/controllers/comments_controller.rb
+++ rails/blog-2/app/controllers/comments_controller.rb
@@ -1,13 +1,32 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+
   respond_to :html, :js, :json
   def index
+    @post = Post.includes(:comments).find(params[:post_id])
+    # render json: @post.comments
+    # respond_to do |format|
+      
+    #   format.js
+    # end
+  end
+
+  def show
     
   end
+
   def create
     post = Post.find(params[:post_id])
     post.comments.create(comments_params)
 
     redirect_to(post)
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to(Post.find(params[:post_id]))
+    
   end
 
   private
